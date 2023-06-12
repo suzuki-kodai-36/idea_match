4f74042a9fcdd3451671b0c589733f8941abe21f
suzuki-kodai <kodai.pri1119@gmail.com>
2023-06-12 14:18:54 +0900

    word_ideasコントローラにindexとcreateアクションを記述

--- app/controllers/word_ideas_controller.rb
+++ app/controllers/word_ideas_controller.rb
@@ -1,4 +1,20 @@
 class WordIdeasController < ApplicationController
   def index
+    # すべてのWordIdeaを取得して、@word_ideasに代入
+    @word_ideas = WordIdea.all
+    # Wordモデルからランダムなレコードidを取得し、セッション変数に格納する
+    # sampleメソッドは、配列からランダムに要素を取得するメソッド
+    session[:random_id] = Word.sample.id
+    # Wordモデルからランダムなレコードidを取得し、@wordに格納する
+    @word = Word.find(session[:random_id])
+  end
+
+  def create
+    @word = Word.find(session[:random_id])
+    # WordIdeaモデルのwordカラムに@wordを、ideaカラムに最新のIdeaを代入する
+    @word_idea = WordIdea.create(word: @word, idea: Idea.last)
+    if @word_idea.save
+      redirect_to word_ideas_path
+    end
   end
 end
